9610785bc9027a10fde802652f6a4a7a3db1b57d
Zakky Muhammad Fajar <zakky.muhammad@sicepat.com>
2022-09-07 20:16:39 +0700

    test: test home function

--- src/screens/__test__/Home.test.tsx
+++ src/screens/__test__/Home.test.tsx
@@ -1,9 +1,22 @@
 import renderer from 'react-test-renderer';
 import Home from '../Home';
+import {homeStore} from '../Home/HomeStore';
 
 describe('Home Screen', () => {
   it('render homescreen correctly', () => {
     const tree = renderer.create(<Home />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('lowercase properly', () => {
+    expect(homeStore.toLowerCase('Hello World')).toBe('hello world');
+  });
+
+  it('uppercase properly', () => {
+    expect(homeStore.toUpperCase('Hello World')).toBe('HELLO WORLD');
+  });
+
+  it('parseString properly', () => {
+    expect(homeStore.parseString('hello world')).toBe('hElLo wOrLd');
   });
 });
